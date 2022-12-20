08a49878ea391ddab54eaea3a7e55e4daeb3d981
RobertLigthart <hello@robertligthart.com>
2022-12-20 16:02:46 +0100

    function from netlify added

--- functions/userdata/userdata.js
+++ functions/userdata/userdata.js
@@ -1,10 +1,23 @@
-exports.handler = async () => {
-    console.log('userdata')
+// exports.handler = async () => {
+//     console.log('userdata')
 
-    const data = {name: 'Robert', age: '69', job: 'chappie'}
+//     const data = {name: 'Robert', age: '69', job: 'chappie'}
 
-    return {
-        status: 200,
-        body: JSON.stringify(data)
-    }
+//     return {
+//         status: 200,
+//         body: JSON.stringify(data)
+//     }
+// }
+
+import { Handler } from '@netlify/functions'
+
+export const handler: Handler = async (event, context) => {
+  const { name = 'stranger' } = event.queryStringParameters
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello, ${name}!`,
+    }),
+  }
 }
